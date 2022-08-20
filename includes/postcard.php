546bc5a2e70c40756c00d370b7329b45229b419a
wrywndp <wiryawanadipa.trityavijaya@gmail.com>
2022-08-20 17:48:02 +0700

    change layout

--- includes/postcard.php
+++ includes/postcard.php
@@ -1,33 +1,34 @@
-<div class="col-md-12 postcard">
-    <div class="row g-0 rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-        <div class="col p-4 d-flex flex-column position-static">
-            <div class="mb-1 text-muted"><?php echo get_the_date('F j, Y'); ?></div>
+<div class="col-xl-3 card border-0 postcard">
+    <div class="row p-2 g-0 rounded overflow-hidden flex-md-row h-md-250 position-relative postcard-inside">
+        <div class="col p-3 d-flex flex-column">
+            <div class="mb-2">
+                <?php
+                $inc = get_post_thumbnail_id(get_the_id());
+                $images = get_children( array (
+                    'post_parent' => $post->ID,
+                    'post_type' => 'attachment',
+                    'post_mime_type' => 'image',
+                    'include' => $inc
+                ));
+                if ( empty($images) ) :
+                else :
+                    foreach ( $images as $attachment ) :
+                        $att_id = $attachment->ID;
+                        $att_title = str_replace('-', ' ', $attachment->post_title);
+                        $att_src = wp_get_attachment_image_src( $attachment->ID, 'bigthumb' );
+                        echo '<img width="' . $att_src[1] . '" height="' . $att_src[2] . '" src="' . $att_src[0] . '" alt="' . $att_title . '" />' . "\n";
+                    endforeach;
+                endif;
+                ?>
+            </div>
+            <div class="text-muted"><?php echo get_the_date('F j, Y'); ?></div>
             <h2>
-                <a class="stretched-link" href="<?php the_permalink() ?>">
+                <a class="fs-5 stretched-link" href="<?php the_permalink() ?>">
                     <?php the_title() ?>
                 </a>
             </h2>
-            <strong class="d-inline-block mb-2 text-primary"><?php echo get_the_category()[0]->name; ?></strong>
-        </div>
-        <div class="col-auto d-none d-md-block">
-            <?php
-            $inc = get_post_thumbnail_id(get_the_id());
-            $images = get_children( array (
-                'post_parent' => $post->ID,
-                'post_type' => 'attachment',
-                'post_mime_type' => 'image',
-                'include' => $inc
-            ));
-            if ( empty($images) ) :
-            else :
-                foreach ( $images as $attachment ) :
-                    $att_id = $attachment->ID;
-                    $att_title = str_replace('-', ' ', $attachment->post_title);
-                    $att_src = wp_get_attachment_image_src( $attachment->ID, 'bigthumb' );
-                    echo '<img width="' . $att_src[1] . '" height="' . $att_src[2] . '" src="' . $att_src[0] . '" alt="' . $att_title . '" />' . "\n";
-                endforeach;
-            endif;
-            ?>
+            <?php the_excerpt(); ?>
+            <span class="d-inline-block mb-2 text-white"><?php echo get_the_category()[0]->name; ?></span>
         </div>
     </div>
 </div>
--- index.php
+++ index.php
@@ -1,16 +1,23 @@
 <?php get_header(); ?>
 <main class="container-xl py-4 main-page">
-	<div class="row">
-		<div class="col-xl-12 ps-xl-4 pe-xl-3">
-		<?php if ( have_posts() ) : ?>
-			<?php while ( have_posts() ) : the_post(); ?>
-				<?php include get_template_directory() . '/includes/postcard.php'; ?>
-			<?php endwhile; ?>
-			<?php else : ?>
-				<?php echo '<p>There are no posts!</p>'; ?>
-			<?php endif; ?>
-			<?php pagenavi(); ?>
+	<div class="row big-article-post-list">
+		<div class="col-lg-6 col-md-12">
+			<div class="big-post px-3">
+				welcome
+			</div>
 		</div>
+		<div class="col-lg-6 col-md-12">
+			welcome
+		</div>
+	</div>
+	<div class="row">
+		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+			<?php include get_template_directory() . '/includes/postcard.php'; ?>
+		<?php endwhile; ?>
+		<?php else : ?>
+			<?php echo '<p>There are no posts!</p>'; ?>
+		<?php endif; ?>
+		<?php pagenavi(); ?>
 	</div>
 </main>
 <?php get_footer(); ?>
