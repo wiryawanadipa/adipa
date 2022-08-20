4d3129bb23f537e7f8c292844f954c392bf9425c
wrywndp <wiryawanadipa.trityavijaya@gmail.com>
2022-08-20 13:36:02 +0700

    move date from bottom to top and move category from top to bottom

--- includes/postcard.php
+++ includes/postcard.php
@@ -1,13 +1,13 @@
 <div class="col-md-12 postcard">
     <div class="row g-0 rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
         <div class="col p-4 d-flex flex-column position-static">
-            <strong class="d-inline-block mb-2 text-primary"><?php echo get_the_category()[0]->name; ?></strong>
+            <div class="mb-1 text-muted"><?php echo get_the_date('F j, Y'); ?></div>
             <h2>
                 <a class="stretched-link" href="<?php the_permalink() ?>">
                     <?php the_title() ?>
                 </a>
             </h2>
-            <div class="mb-1 text-muted"><?php echo get_the_date('F j, Y'); ?></div>
+            <strong class="d-inline-block mb-2 text-primary"><?php echo get_the_category()[0]->name; ?></strong>
         </div>
         <div class="col-auto d-none d-md-block">
             <?php
