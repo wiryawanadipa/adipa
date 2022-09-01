88ae1738258ec8b35be9532eb0fe96a6819e7cb7
wrywndp <wiryawanadipa.trityavijaya@gmail.com>
2022-09-01 18:31:06 +0700

    prevent form from resubmit when refresh

--- template-parts/pages/contact.php
+++ template-parts/pages/contact.php
@@ -1,68 +1,74 @@
 <?php
+session_start();
 if(isset($_POST['submit'])) {
-	if(trim($_POST['contactName']) === '') {
-		$emptyName = 'Please enter your name.';
-		$emptyNameError = true;
-	} else {
-		$name = trim($_POST['contactName']);
-	}
-
-	if(trim($_POST['email']) === '')  {
-		$emptyEmail = 'Please enter your email address.';
-		$emptyEmailError = true;
-	} else if (!preg_match("/^[[:alnum:]][a-z0-9_.-]*@[a-z0-9.-]+\.[a-z]{2,4}$/i", trim($_POST['email']))) {
-		$invalidEmail = 'You entered an invalid email format address. (e.g. yourname@domain.tld).';
-		$invalidEmailError = true;
-	} else {
-		$email = trim($_POST['email']);
-	}
-
-	if(trim($_POST['comments']) === '') {
-		$emptyCommentError = true;
-		$emptyComment = 'Please enter a message.';
-	} else {
-		if(function_exists('stripslashes')) {
-			$comments = stripslashes(trim($_POST['comments']));
+	if ($_SESSION['rand'] == $_POST['randcheck']) {
+		if(trim($_POST['contactName']) === '') {
+			$emptyName = 'Please enter your name.';
+			$emptyNameError = true;
 		} else {
-			$comments = trim($_POST['comments']);
-		}   
-	}
+			$name = trim($_POST['contactName']);
+		}
 
-	if(!empty($_POST['g-recaptcha-response'])){
-		$secret = get_option( 'wa_recaptcha' );
-		$ip = $_SERVER['REMOTE_ADDR'];
-		$captcha = $_POST['g-recaptcha-response'];
-		$rsp = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret=' . $secret . '&response=' . $captcha .'&remoteip='.$ip);
-		$array = json_decode($rsp, TRUE);
-		if($array["success"]) {
+		if(trim($_POST['email']) === '')  {
+			$emptyEmail = 'Please enter your email address.';
+			$emptyEmailError = true;
+		} else if (!preg_match("/^[[:alnum:]][a-z0-9_.-]*@[a-z0-9.-]+\.[a-z]{2,4}$/i", trim($_POST['email']))) {
+			$invalidEmail = 'You entered an invalid email format address. (e.g. yourname@domain.tld).';
+			$invalidEmailError = true;
+		} else {
+			$email = trim($_POST['email']);
 		}
-	} else {
-		echo '<div class="p-3 mb-2 bg-danger rounded-1"><i class="fa-solid fa-triangle-exclamation"></i> Please check the captcha.</div>';
-	}
 
-	if(!isset($emptyNameError) && !isset($emptyEmailError) && !isset($invalidEmailError) && !isset($emptyCommentError) && !empty($_POST['g-recaptcha-response'])) {
-		$emailTo = get_option( 'wa_mail' );;
-		$subject = '[' . $_SERVER['HTTP_HOST'] . '] From '.$name;
-		$body = 'Name: ' . $name . "\n\n" . 'Email:' . $email . "\n\n" . 'Comments:' . $comments;
-		$headers = 'From: '.$name.' <'.$emailTo.'>' . "\r\n" . 'Reply-To: ' . $email;
-		wp_mail($emailTo, $subject, $body, $headers);
-		$emailSent = true;
-		echo '<div class="p-3 mb-2 bg-success rounded-1">Thank you for contacting me! Your message has been sent. I&lsquo;ll respond to you within 2x24 hours</div>';
-	} else {
-		if (isset($emptyNameError)) {
-			echo '<div class="p-3 mb-2 bg-danger rounded-1"><i class="fa-solid fa-triangle-exclamation"></i> ' . $emptyName . '</div>';
+		if(trim($_POST['comments']) === '') {
+			$emptyCommentError = true;
+			$emptyComment = 'Please enter a message.';
+		} else {
+			if(function_exists('stripslashes')) {
+				$comments = stripslashes(trim($_POST['comments']));
+			} else {
+				$comments = trim($_POST['comments']);
+			}   
 		}
-		if (isset($emptyEmailError)) {
-			echo '<div class="p-3 mb-2 bg-danger rounded-1"><i class="fa-solid fa-triangle-exclamation"></i> ' . $emptyEmail . '</div>';
+
+		if(!empty($_POST['g-recaptcha-response'])){
+			$secret = get_option( 'wa_recaptcha' );
+			$ip = $_SERVER['REMOTE_ADDR'];
+			$captcha = $_POST['g-recaptcha-response'];
+			$rsp = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret=' . $secret . '&response=' . $captcha .'&remoteip='.$ip);
+			$array = json_decode($rsp, TRUE);
+			if($array["success"]) {
+			}
+		} else {
+			echo '<div class="p-3 mb-2 bg-danger rounded-1"><i class="fa-solid fa-triangle-exclamation"></i> Please check the captcha.</div>';
 		}
-		if (isset($invalidEmailError)) {
-			echo '<div class="p-3 mb-2 bg-danger rounded-1"><i class="fa-solid fa-triangle-exclamation"></i> ' . $invalidEmail . '</div>';
+
+		if(!isset($emptyNameError) && !isset($emptyEmailError) && !isset($invalidEmailError) && !isset($emptyCommentError) && !empty($_POST['g-recaptcha-response'])) {
+			$emailTo = get_option( 'wa_mail' );;
+			$subject = '[wiryawanadipa.com] From '.$name;
+			$body = 'Name: ' . $name . "\n\n" . 'Email:' . $email . "\n\n" . 'Comments:' . $comments;
+			$headers = 'From: '.$name.' <'.$emailTo.'>' . "\r\n" . 'Reply-To: ' . $email;
+			wp_mail($emailTo, $subject, $body, $headers);
+			$emailSent = true;
+			echo '<div class="p-3 mb-2 bg-success rounded-1">Thank you for contacting me! Your message has been sent. I&lsquo;ll respond to you within 2x24 hours</div>';
+		} else {
+			if (isset($emptyNameError)) {
+				echo '<div class="p-3 mb-2 bg-danger rounded-1"><i class="fa-solid fa-triangle-exclamation"></i> ' . $emptyName . '</div>';
+			}
+			if (isset($emptyEmailError)) {
+				echo '<div class="p-3 mb-2 bg-danger rounded-1"><i class="fa-solid fa-triangle-exclamation"></i> ' . $emptyEmail . '</div>';
+			}
+			if (isset($invalidEmailError)) {
+				echo '<div class="p-3 mb-2 bg-danger rounded-1"><i class="fa-solid fa-triangle-exclamation"></i> ' . $invalidEmail . '</div>';
+			}
+			if (isset($emptyCommentError)) {
+				echo '<div class="p-3 mb-2 bg-danger rounded-1"><i class="fa-solid fa-triangle-exclamation"></i> ' . $emptyComment . '</div>';
+			}
 		}
-		if (isset($emptyCommentError)) {
-			echo '<div class="p-3 mb-2 bg-danger rounded-1"><i class="fa-solid fa-triangle-exclamation"></i> ' . $emptyComment . '</div>';
-		}
+	} elseif ($_SESSION['rand'] != $_POST['randcheck']) {
+		echo '<div class="p-3 mb-2 bg-danger rounded-1"><i class="fa-solid fa-triangle-exclamation"></i> Please fill the form and check the captcha</div>';
 	}
 }
+
 ?>
 <div class="col-12 py-4">
 	<script src="https://www.google.com/recaptcha/api.js?hl=en" async defer></script>
@@ -91,6 +97,11 @@ if(isset($_POST['submit'])) {
 				</div>
 				<div class="form-group mb-4">
 					<div class="col-12">
+						<?php 
+						$rand = rand();
+						$_SESSION['rand'] = $rand;
+						?>
+						<input type="hidden" value="<?php echo $rand; ?>" name="randcheck" />
 						<button type="submit"  name="submit" value="Send" class="btn btn-primary">Send Message</button>
 					</div>
 				</div>
