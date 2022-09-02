aca6855603c110e0316b98ca70e25218d9ddcce8
wrywndp <wiryawanadipa.trityavijaya@gmail.com>
2022-09-02 16:46:39 +0700

    change footer name to domain

--- template-parts/footer.php
+++ template-parts/footer.php
@@ -5,7 +5,7 @@
                     <div class="col-12 col-md-6 fs-1 mb-3 mb-md-0 text-center text-md-start">Thanks for visiting!</div>
                     <!-- Social Links -->
                     <?php get_template_part( 'template-parts/parts/social' ); ?>
-                    <div class="col-12 col-md-6 mb-1 small-text text-center text-md-start"><i class="fa-solid fa-copyright"></i> <?php echo date( 'Y' ); ?>, <?php echo strtoupper($_SERVER['HTTP_HOST']); ?></div>
+                    <div class="col-12 col-md-6 mb-1 small-text text-center text-md-start"><i class="fa-solid fa-copyright"></i> <?php echo date( 'Y' ); ?>, <?php echo $_SERVER['HTTP_HOST']; ?></div>
                     <div class="col-12 col-md-6"></div>
                     <div class="col-12 col-md-6 small-text text-muted pt-2 pt-md-0 text-center text-md-start"><i class="fa-solid fa-building-shield"></i><a class="small-text text-muted privacy-policy" href="<?php bloginfo('url'); ?>/privacy-policy/" target="_blank"> Privacy Policy</a></div>
                     <div class="col-12 col-md-6"></div>
