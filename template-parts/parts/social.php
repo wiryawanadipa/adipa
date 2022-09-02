79c1f1a1d94705dcc6ff0145ca81f83031260250
wrywndp <wiryawanadipa.trityavijaya@gmail.com>
2022-09-02 23:31:22 +0700

    add title attribute to some link

--- template-parts/parts/social.php
+++ template-parts/parts/social.php
@@ -8,12 +8,12 @@
     $youtube = trim( get_option( 'wa_youtube' ) );
     $medium = trim( get_option( 'wa_medium' ) );
     
-    echo '<a rel="nofollow" class="mx-sm-1 p-1" href="https://www.facebook.com/' . get_option( 'wa_facebook', 'wiryawanadipa' ) . '/" target="_blank"><i class="fa-brands fa-square-facebook fa-2x"></i></a>' . "\n";
-    echo '<a rel="nofollow" class="mx-sm-1 p-1" href="https://twitter.com/' . get_option( 'wa_twitter', 'wiryawanadipa' ) . '" target="_blank"><i class="fa-brands fa-square-twitter fa-2x"></i></a>' . "\n";
-    echo '<a rel="nofollow" class="mx-sm-1 p-1" href="https://www.instagram.com/' . get_option( 'wa_instagram', 'wiryawanadipa' ) . '/" target="_blank"><i class="fa-brands fa-square-instagram fa-2x"></i></a>' . "\n";
-    echo '<a rel="nofollow" class="mx-sm-1 p-1" href="https://www.linkedin.com/in/' . get_option( 'wa_linkedin', 'wiryawanadipa' ) . '/" target="_blank"><i class="fa-brands fa-linkedin fa-2x"></i></a>' . "\n";
-    echo '<a rel="nofollow" class="mx-sm-1 p-1" href="https://github.com/' . get_option( 'wa_github', 'wiryawanadipa' ) . '" target="_blank"><i class="fa-brands fa-square-github fa-2x"></i></a>' . "\n";
-    echo '<a rel="nofollow" class="mx-sm-1 p-1" href="https://www.youtube.com/channel/' . get_option( 'wa_youtube', 'UCpP1g9Vcl33ucu5mO2vr-5Q' ) . '" target="_blank"><i class="fa-brands fa-youtube fa-2x"></i></a>' . "\n";
-    echo '<a rel="nofollow" class="mx-sm-1 p-1" href="https://medium.com/@' . get_option( 'wa_medium', 'wiryawanadipa' ) . '" target="_blank"><i class="fa-brands fa-medium fa-2x"></i></a>' . "\n";
+    echo '<a rel="nofollow" title="' . get_bloginfo( 'name' ) . ' Facebook" class="mx-sm-1 p-1" href="https://www.facebook.com/' . get_option( 'wa_facebook', 'wiryawanadipa' ) . '/" target="_blank"><i class="fa-brands fa-square-facebook fa-2x"></i></a>' . "\n";
+    echo '<a rel="nofollow" title="' . get_bloginfo( 'name' ) . ' Twitter" class="mx-sm-1 p-1" href="https://twitter.com/' . get_option( 'wa_twitter', 'wiryawanadipa' ) . '" target="_blank"><i class="fa-brands fa-square-twitter fa-2x"></i></a>' . "\n";
+    echo '<a rel="nofollow" title="' . get_bloginfo( 'name' ) . ' Instagram" class="mx-sm-1 p-1" href="https://www.instagram.com/' . get_option( 'wa_instagram', 'wiryawanadipa' ) . '/" target="_blank"><i class="fa-brands fa-square-instagram fa-2x"></i></a>' . "\n";
+    echo '<a rel="nofollow" title="' . get_bloginfo( 'name' ) . ' LinkedIn" class="mx-sm-1 p-1" href="https://www.linkedin.com/in/' . get_option( 'wa_linkedin', 'wiryawanadipa' ) . '/" target="_blank"><i class="fa-brands fa-linkedin fa-2x"></i></a>' . "\n";
+    echo '<a rel="nofollow" title="' . get_bloginfo( 'name' ) . ' GitHub" class="mx-sm-1 p-1" href="https://github.com/' . get_option( 'wa_github', 'wiryawanadipa' ) . '" target="_blank"><i class="fa-brands fa-square-github fa-2x"></i></a>' . "\n";
+    echo '<a rel="nofollow" title="' . get_bloginfo( 'name' ) . ' Youtube Channel" class="mx-sm-1 p-1" href="https://www.youtube.com/channel/' . get_option( 'wa_youtube', 'UCpP1g9Vcl33ucu5mO2vr-5Q' ) . '" target="_blank"><i class="fa-brands fa-youtube fa-2x"></i></a>' . "\n";
+    echo '<a rel="nofollow" title="' . get_bloginfo( 'name' ) . ' Medium" class="mx-sm-1 p-1" href="https://medium.com/@' . get_option( 'wa_medium', 'wiryawanadipa' ) . '" target="_blank"><i class="fa-brands fa-medium fa-2x"></i></a>' . "\n";
     ?>
 </div>
