b91271f57e351b72ab17cca3a84983aab1a63d6b
wrywndp <wiryawanadipa.trityavijaya@gmail.com>
2022-09-02 22:15:01 +0700

    change checkmark class

--- template-parts/parts/topnav.php
+++ template-parts/parts/topnav.php
@@ -8,20 +8,20 @@
             </button>
             <div class="collapse navbar-collapse mt-1 mt-md-0" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto mb-2 mb-md-0 me-md-2 mt-3 mt-md-0">
-                    <li class="nav-item mx-lg-1">
-                        <a class="nav-link fs-5 wa-top-menu" href="<?php bloginfo('url'); ?>">home</a>
+                    <li class="nav-item mx-md-1">
+                        <a class="nav-link fs-5 wa-top-menu<?php if ( is_home() ) { echo ' menu-active'; } ?>" href="<?php bloginfo('url'); ?>">home</a>
                     </li>
-                    <li class="nav-item mx-lg-1">
-                        <a class="nav-link fs-5 wa-top-menu" href="<?php bloginfo('url'); ?>/about/">about</a>
+                    <li class="nav-item mx-md-1">
+                        <a class="nav-link fs-5 wa-top-menu<?php if ( is_page( 'About' ) ) { echo ' menu-active'; } ?>" href="<?php bloginfo('url'); ?>/about/">about</a>
                     </li>
-                    <li class="nav-item mx-lg-1">
-                        <a class="nav-link fs-5 wa-top-menu" href="<?php bloginfo('url'); ?>/contact/">contact</a>
+                    <li class="nav-item mx-md-1">
+                        <a class="nav-link fs-5 wa-top-menu<?php if ( is_page( 'Contact' ) ) { echo ' menu-active'; } ?>" href="<?php bloginfo('url'); ?>/contact/">contact</a>
                     </li>
-                    <li class="nav-item mx-lg-1 dropdown">
+                    <li class="nav-item mx-md-1 dropdown">
                         <a class="nav-link dropdown-toggle p-3 p-md-2" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fa-solid fa-magnifying-glass"></i></a>
                         <ul class="dropdown-menu dropdown-menu-end">
                             <form action="<?php bloginfo('url'); ?>" role="search" method="get">
-                                <input type="search" name="s" autocomplete="off" placeholder="Search here..." title="Search" required>
+                                <input type="search" class="check" name="s" autocomplete="off" placeholder="Search here..." title="Search" required>
                                 <button type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
                             </form>
                         </ul>
