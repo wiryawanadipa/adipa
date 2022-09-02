10d5e8c14685eb9bf4afc2afaaddca89755a8da6
wrywndp <wiryawanadipa.trityavijaya@gmail.com>
2022-09-02 21:36:33 +0700

    add check mark for input

--- template-parts/parts/contact-form.php
+++ template-parts/parts/contact-form.php
@@ -77,10 +77,10 @@ if( null != get_option( 'wa_recaptcha' ) && !empty( get_option( 'wa_recaptcha' )
 				<fieldset>
 					<div class="row mb-0 mb-md-4">
 						<div class="col-12 col-md-6 mb-4 mb-md-0">
-							<input id="cf-name" name="contactName" type="text" placeholder="Please enter your full name here." class="form-control" value="<?php if( isset( $_POST['contactName'] ) ){ echo esc_attr($_POST['contactName']);} else { echo '';} ?>" required>
+							<input id="cf-name" class="form-control" name="contactName" type="text" placeholder="Please enter your full name here." value="<?php if( isset( $_POST['contactName'] ) ){ echo esc_attr($_POST['contactName']);} else { echo '';} ?>" required>
 						</div>
 						<div class="col-12 col-md-6 mb-4 mb-md-0">
-							<input id="cf-email" name="email" type="email" placeholder="Please enter your e-mail address here." class="form-control" value="<?php if( isset( $_POST['email'] ) ){ echo esc_attr($_POST['email']);} else { echo '';} ?>" required>
+							<input id="cf-email" class="form-control" name="email" type="email" placeholder="Please enter your e-mail address here." value="<?php if( isset( $_POST['email'] ) ){ echo esc_attr($_POST['email']);} else { echo '';} ?>" required>
 						</div>
 					</div>
 					<div class="row mb-4">
--- template-parts/parts/quote.php
+++ template-parts/parts/quote.php
@@ -8,8 +8,8 @@
 		<div class="row justify-content-end"> 
 		<div class="col-12 col-lg-5">
 		<blockquote>
-			<p class="text-center text-sm-start">Overdesigned UIs that look better than they function need to die.</p>
-			<p class="text-center text-sm-end fst-italic"><a rel="nofollow" href="https://twitter.com/Buildzoid1/status/1556027650036142082" target="_blank">Buildzoid</a>, 2022</p>
+			<span class="text-center text-sm-start">Overdesigned UIs that look better than they function need to die.</span>
+			<span class="text-center text-sm-end fst-italic"><a rel="nofollow" href="https://twitter.com/Buildzoid1/status/1556027650036142082" target="_blank">Buildzoid</a>, 2022</span>
 		</blockquote>
 		</div>
 		</div>
