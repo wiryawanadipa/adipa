586df0d707d6851dad884a9321baf3a45ddc9a3f
wrywndp <wiryawanadipa.trityavijaya@gmail.com>
2022-09-04 01:45:20 +0700

    change homepage blog post

--- home.php
+++ home.php
@@ -3,12 +3,18 @@
 	<?php get_template_part( 'template-parts/parts/hero' ); ?>
 	<?php get_template_part( 'template-parts/parts/design-gallery' ); ?>
 <?php } ?>
-<main class="container-xl py-4 py-xl-5<?php if ( !is_paged() ) { ?> mt-5<?php } ?> main-page">
+<main class="container-xl py-4 py-xl-5 main-page">
+	<h2 class="fs-1 text-center mb-4 mb-sm-5 text-white title-decoration">blog</h2>
 	<div class="row mb-5 g-2 g-md-3 g-xl-2 g-xxl-3">
 		<?php
 		if ( have_posts() ) {
-			while ( have_posts() ) {
-				the_post();
+			$blog = array(
+				'posts_per_page' => 9,
+				'category_name' => 'Blog'
+			);
+				$loop = new WP_Query( $blog );
+			while ( $loop->have_posts() ) {
+				$loop->the_post();
 				get_template_part( 'template-parts/parts/postcard' );
 			}
 		} else {
@@ -16,6 +22,5 @@
 		}
 		?>
 	</div>
-	<?php pagenavi(); ?>
 </main>
 <?php get_template_part( 'template-parts/footer' ); ?>
