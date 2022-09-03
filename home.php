8ce17cd20360b89003989d2da6f52976e3127d80
wrywndp <wiryawanadipa.trityavijaya@gmail.com>
2022-09-04 01:53:56 +0700

    move wp_query up

--- home.php
+++ home.php
@@ -7,12 +7,12 @@
 	<h2 class="fs-1 text-center mb-4 mb-sm-5 text-white title-decoration">blog</h2>
 	<div class="row mb-5 g-2 g-md-3 g-xl-2 g-xxl-3">
 		<?php
-		if ( have_posts() ) {
-			$blog = array(
-				'posts_per_page' => 9,
-				'category_name' => 'Blog'
-			);
-				$loop = new WP_Query( $blog );
+		$blog = array(
+			'posts_per_page' => 9,
+			'category_name' => 'Blog'
+		);
+		$loop = new WP_Query( $blog );
+		if ( $loop->have_posts() ) {
 			while ( $loop->have_posts() ) {
 				$loop->the_post();
 				get_template_part( 'template-parts/parts/postcard' );
