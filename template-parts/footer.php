b5b0070225ddc122f63de60724a42bfc2a773ab0
wrywndp <wiryawanadipa.trityavijaya@gmail.com>
2022-09-04 19:33:46 +0700

    nofollow

--- template-parts/footer.php
+++ template-parts/footer.php
@@ -8,7 +8,7 @@
                     <div class="col-12 col-md-6"></div>
                     <div class="col-12 col-md-6 mb-1 small-text text-muted pt-2 pt-md-0 text-center text-md-start"><i class="fa-solid fa-building-shield"></i><a class="small-text text-muted privacy-policy" href="<?php echo esc_url( get_page_link( get_page_id_by_title( 'Privacy Policy' ) ) ); ?>"> Privacy Policy</a></div>
                     <div class="col-12 col-md-6"></div>
-                    <div class="col-12 col-md-6 mb-1 small-text text-muted pt-2 pt-md-0 text-center text-md-start"><i class="fa-brands fa-html5"></i> <a class="small-text text-muted theme-version" title="Wiryawan Adipa Wordpress Theme GitHub Repository" href="https://github.com/wiryawanadipa/adipa" target="_blank">Theme by Wiryawan Adipa v<?php $theme_version = wp_get_theme(); echo $theme_version->Version; ?></a></div>
+                    <div class="col-12 col-md-6 mb-1 small-text text-muted pt-2 pt-md-0 text-center text-md-start"><i class="fa-brands fa-html5"></i> <a rel="nofollow" class="small-text text-muted theme-version" title="Wiryawan Adipa Wordpress Theme GitHub Repository" href="https://github.com/wiryawanadipa/adipa" target="_blank">Theme by Wiryawan Adipa v<?php $theme_version = wp_get_theme(); echo $theme_version->Version; ?></a></div>
                 </div>
             </div>
         </div>
