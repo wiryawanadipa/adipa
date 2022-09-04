eb1255976ffd574f2658e250049f625eda7258b5
wrywndp <wiryawanadipa.trityavijaya@gmail.com>
2022-09-04 14:32:57 +0700

    about page edit

--- page-templates/about.php
+++ page-templates/about.php
@@ -22,7 +22,8 @@
                             }
                         }
                     ?>
-                    <div class="fs-3 about social">
+                    <div class="fs-3 text-center about social">
+                        <a title="<?php get_bloginfo( 'name' ); ?> Mail" class="mx-sm-1 p-1" href="<?php echo esc_url( get_page_link( get_page_id_by_title( 'Contact' ) ) ); ?>"><i class="fa-solid fa-envelope fa-2x"></i></a>
                         <?php get_template_part( 'template-parts/parts/social' ); ?>
                     </div>
                 </div>
