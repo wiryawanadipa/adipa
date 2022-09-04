6bdd7ec1d5fc6bf986ee83ef64d3db96a66cfebc
wrywndp <wiryawanadipa.trityavijaya@gmail.com>
2022-09-04 19:21:42 +0700

    remove unnecessary variable

--- functions.php
+++ functions.php
@@ -48,16 +48,12 @@ if( isset( $_GET['activated'] ) && is_admin() ) {
 // Add breadcrumbs
 function breadcrumbs() {
 	global $post;
-	$schema_link = 'http://schema.org/ListItem';
-	$home = 'Home';
-	$delimiter = ' &gt; ';
-	$homeLink = get_bloginfo('url');
 	echo '<ol class="container p-3 py-2 rounded-3 mb-3 breadcrumbs" itemscope itemtype="http://schema.org/BreadcrumbList">' . "\n";
-	echo '<li itemprop="itemListElement" itemscope itemtype="' . $schema_link . '"><a itemprop="item" href="' . $homeLink . '">' . '<span itemprop="name">' . $home . '</span>' . '</a><meta itemprop="position" content="1" /></li>' . $delimiter . "\n";
+	echo '<li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem"><a itemprop="item" href="' . get_bloginfo('url') . '">' . '<span itemprop="name">Home</span>' . '</a><meta itemprop="position" content="1" /></li> &gt; ' . "\n";
 	$categories = wp_get_post_terms( $post->ID, 'category', array( 'orderby' => 'parent', 'order' => 'ASC' ) );
 	if ( $categories ) {
 		foreach ( $categories as $cat ) {
-			echo '<li itemprop="itemListElement" itemscope itemtype="' . $schema_link . '"><a itemprop="item" href="' . get_category_link($cat->term_id) . '">' . '<span itemprop="name">' . $cat->name . '</span>' . '</a><meta itemprop="position" content="2" /></li>' . $delimiter . "\n";
+			echo '<li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem"><a itemprop="item" href="' . get_category_link($cat->term_id) . '">' . '<span itemprop="name">' . $cat->name . '</span>' . '</a><meta itemprop="position" content="2" /></li> &gt; ' . "\n";
 		}
 	}
 	echo '<li>' . get_the_title() . '</li>' . "\n";
