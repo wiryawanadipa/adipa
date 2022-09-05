564dd875cf98204973f586b9505ab7b183936a6d
wrywndp <wiryawanadipa.trityavijaya@gmail.com>
2022-09-05 16:44:07 +0700

    remove png, use base64 instead

--- template-parts/parts/hero.php
+++ template-parts/parts/hero.php
@@ -13,7 +13,7 @@
 				<div class="hero-heading-text text-center text-sm-start"><?php do_action( 'hero_desc' ); ?></div>
 			</div>
 			<div class="col-lg-4 d-none d-lg-block position-relative hero-photo">
-				<img class="position-absolute top-50 start-50 translate-middle rounded-circle" src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/logo/wiryawan-adipa-logo-big.png" width="256px" height="256px" alt="Wiryawan Adipa Logo Big">
+				<img class="position-absolute top-50 start-50 translate-middle rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==" width="256px" height="256px" alt="wa logo big">
 			</div>
 		</div>
 	</div>
