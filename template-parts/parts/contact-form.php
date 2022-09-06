df4c22ca596c41603c05a6bad767e3f170d2d4ef
wrywndp <wiryawanadipa.trityavijaya@gmail.com>
2022-09-06 14:12:19 +0700

    add limit to form and add counter

--- template-parts/parts/contact-form.php
+++ template-parts/parts/contact-form.php
@@ -4,29 +4,35 @@ if( null != get_option( 'wa_recaptcha_site_key' ) && !empty( get_option( 'wa_rec
 		if ($_SESSION['rand'] == $_POST['randcheck']) {
 			if(trim($_POST['contactName']) === '') {
 				$emptyNameError = true;
+			} else if (strlen(trim($_POST['contactName'])) > 50) {
+				$longNameError = true;
 			} else {
 				$name = trim($_POST['contactName']);
 			}
 
 			if(trim($_POST['email']) === '')  {
 				$emptyEmailError = true;
+			} else if (strlen(trim($_POST['email'])) > 80) {
+				$longEmailError = true;
 			} else if (!preg_match("/^[[:alnum:]][a-z0-9_.-]*@[a-z0-9.-]+\.[a-z]{2,6}$/i", trim($_POST['email']))) {
 				$invalidEmailError = true;
 			} else {
 				$email = trim($_POST['email']);
 			}
 
-			if(trim($_POST['comments']) === '') {
-				$emptyCommentError = true;
+			if(trim($_POST['message']) === '') {
+				$emptyMessageError = true;
+			} else if (strlen(trim($_POST['message'])) > 1000) {
+				$longMessageError = true;
 			} else {
 				if(function_exists('stripslashes')) {
-					$comments = stripslashes(trim($_POST['comments']));
+					$message = stripslashes(trim($_POST['message']));
 				} else {
-					$comments = trim($_POST['comments']);
+					$message = trim($_POST['message']);
 				}   
 			}
 
-			if(!isset($emptyNameError) && !isset($emptyEmailError) && !isset($invalidEmailError) && !isset($emptyCommentError) && !empty($_POST['g-recaptcha-response'])) {
+			if(!isset($emptyNameError) && strlen(trim($_POST['contactName'])) < 51 && !isset($emptyEmailError) && strlen(trim($_POST['email'])) < 81 && !isset($invalidEmailError) && !isset($emptyMessageError) && strlen(trim($_POST['message'])) < 1001 && !empty($_POST['g-recaptcha-response'])) {
 				$secret = get_option( 'wa_recaptcha_secret_key' );
 				$ip = $_SERVER['REMOTE_ADDR'];
 				$captcha = $_POST['g-recaptcha-response'];
@@ -36,7 +42,7 @@ if( null != get_option( 'wa_recaptcha_site_key' ) && !empty( get_option( 'wa_rec
 					$emailTo = get_option( 'wa_mail' );
 					$domain = strtoupper($_SERVER['HTTP_HOST']);
 					$subject = '[' . $domain . '] From '.$name;
-					$body = 'Name: ' . $name . "\n\n" . 'Email:' . $email . "\n\n" . 'Comments:' . $comments;
+					$body = 'Name: ' . $name . "\n\n" . 'Email:' . $email . "\n\n" . 'Message:' . $message;
 					$headers = 'From: '.$name.' <'.$emailTo.'>' . "\r\n" . 'Reply-To: ' . $email;
 					$mail = wp_mail($emailTo, $subject, $body, $headers);
 					if($mail) {
@@ -52,14 +58,23 @@ if( null != get_option( 'wa_recaptcha_site_key' ) && !empty( get_option( 'wa_rec
 				if (isset($emptyNameError)) {
 					echo '<div class="p-3 mb-2 bg-danger rounded-1"><i class="fa-solid fa-triangle-exclamation"></i> Please enter your name.</div>';
 				}
+				if (isset($longNameError)) {
+					echo '<div class="p-3 mb-2 bg-danger rounded-1"><i class="fa-solid fa-triangle-exclamation"></i> The name that you filled in the name form is too long. The name form should be no more than 50 characters.</div>';
+				}
 				if (isset($emptyEmailError)) {
 					echo '<div class="p-3 mb-2 bg-danger rounded-1"><i class="fa-solid fa-triangle-exclamation"></i> Please enter your email address.</div>';
 				}
 				if (isset($invalidEmailError)) {
 					echo '<div class="p-3 mb-2 bg-danger rounded-1"><i class="fa-solid fa-triangle-exclamation"></i> You entered an invalid email format address. (e.g. yourname@domain.tld).</div>';
 				}
-				if (isset($emptyCommentError)) {
+				if (isset($longEmailError)) {
+					echo '<div class="p-3 mb-2 bg-danger rounded-1"><i class="fa-solid fa-triangle-exclamation"></i> Your email address is too long. Use an email address which is no more than 80 characters.</div>';
+				}
+				if (isset($emptyMessageError)) {
 					echo '<div class="p-3 mb-2 bg-danger rounded-1"><i class="fa-solid fa-triangle-exclamation"></i> Please enter a message.</div>';
+				}
+				if (isset($longMessageError)) {
+					echo '<div class="p-3 mb-2 bg-danger rounded-1"><i class="fa-solid fa-triangle-exclamation"></i> Your message is too long. Message should be no more than 1000 characters.</div>';
 				}
 				if(empty($_POST['g-recaptcha-response'])) {
 					echo '<div class="p-3 mb-2 bg-danger rounded-1"><i class="fa-solid fa-triangle-exclamation"></i> Please check the captcha.</div>';
@@ -77,20 +92,44 @@ if( null != get_option( 'wa_recaptcha_site_key' ) && !empty( get_option( 'wa_rec
 				<fieldset>
 					<div class="row mb-0 mb-md-3">
 						<div class="col-12 col-md-6 mb-3 mb-md-0">
-							<label class="mb-2">Name</label>
-							<input id="cf-name" class="form-control check" name="contactName" type="text" placeholder="Please enter your name here." value="<?php if( isset( $_POST['contactName'] ) && !isset($emailSent) ){ echo esc_attr($_POST['contactName']);} else { echo '';} ?>" autofocus required>
+							<label class="mb-2">Name<span class="text-danger">&#42;</span></label>
+							<input id="cf-name" class="form-control check" name="contactName" type="text" placeholder="Please enter your name here." maxlength="50" value="<?php if( isset( $_POST['contactName'] ) && !isset($emailSent) ) { echo esc_attr($_POST['contactName']);} else { echo '';} ?>" autofocus required>
 						</div>
 						<div class="col-12 col-md-6 mb-3 mb-md-0">
-							<label class="mb-2">E-Mail</label>
-							<input id="cf-email" class="form-control check" name="email" type="email" placeholder="Please enter your e-mail address here." value="<?php if( isset( $_POST['email'] ) && !isset($emailSent) ){ echo esc_attr($_POST['email']);} else { echo '';} ?>" required>
+							<label class="mb-2">E-Mail<span class="text-danger">&#42;</span></label>
+							<input id="cf-email" class="form-control check" name="email" type="email" placeholder="Please enter your e-mail address here." maxlength="80" value="<?php if( isset( $_POST['email'] ) && !isset($emailSent) ) { echo esc_attr($_POST['email']);} else { echo '';} ?>" required>
 						</div>
 					</div>
 					<div class="row mb-3">
 						<div class="col-12">
-							<label class="mb-2">Message</label>
-							<textarea class="form-control check" id="comments" placeholder="Please enter your message here." name="comments" rows="10" class="form-control" required><?php if( isset( $_POST['comments'] ) && !isset($emailSent) ){ echo esc_attr($_POST['comments']);} else { echo '';} ?></textarea>
+							<label class="mb-2">Message<span class="text-danger">&#42;</span></label>
+							<textarea class="form-control check" id="message" placeholder="Please enter your message here." maxlength="1000" name="message" rows="10" class="form-control" required><?php if( isset( $_POST['message'] ) && !isset($emailSent) ) { echo esc_attr($_POST['message']);} else { echo '';} ?></textarea>
 						</div>
 					</div>
+					<div id="messagecharcounter" class="mb-3 text-end text-light">
+						<span id="typedchar"><?php if( isset( $_POST['message'] ) && !isset($emailSent) ) { echo strlen(trim($_POST['message']));} else { echo '0';} ?></span>
+						<span id="maxchar">/ 1000</span>
+					</div>
+					<script>
+					const messageElement = document.querySelector("#message");
+					const characterCounterElement = document.querySelector("#messagecharcounter");
+					const typedCharElement = document.querySelector("#typedchar");
+					const maxChar = 1000;
+					message.addEventListener("input", event => {
+						const typedChar = message.value.length;
+						if (typedChar > maxChar) {
+							return false;
+						}
+						typedCharElement.textContent = typedChar;
+						if (typedChar > maxChar-1) {
+							characterCounterElement.classList = "mb-3 text-end text-danger";
+						} else if (typedChar < maxChar && typedChar > maxChar-100) {
+							characterCounterElement.classList = "mb-3 text-end text-warning";
+						} else if (typedChar < maxChar-99) {
+							characterCounterElement.classList = "mb-3 text-end text-light";
+						}
+					});
+					</script>
 					<div class="row mb-4">
 						<div class="col-12 col-md-6 mb-4 mb-md-0">
 							<div class="g-recaptcha brochure__form__captcha" data-sitekey="<?php echo get_option( 'wa_recaptcha_site_key' ); ?>"></div>
