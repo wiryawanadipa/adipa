be75b6db44a46c52ecf8889e91e09b649662df39
wrywndp <wiryawanadipa.trityavijaya@gmail.com>
2022-09-09 11:38:05 +0700

    update using content

--- template-parts/parts/quote.php
+++ template-parts/parts/quote.php
@@ -8,8 +8,8 @@
 		<div class="row justify-content-end"> 
 		<div class="col-12 col-lg-5">
 		<blockquote>
-			<span class="text-center text-sm-start">Overdesigned UIs that look better than they function need to die.</span>
-			<span class="text-center text-sm-end fst-italic"><a rel="nofollow" title="Buildzoid's Twitter Post Link" href="https://twitter.com/Buildzoid1/status/1556027650036142082" target="_blank">Buildzoid</a>, 2022</span>
+			<span></span>
+			<span class="text-center text-sm-end fst-italic"><a rel="nofollow noopener noreferrer" title="Buildzoid's Twitter Post Link" href="https://twitter.com/Buildzoid1/status/1556027650036142082" target="_blank">Buildzoid</a>, 2022</span>
 		</blockquote>
 		</div>
 		</div>
