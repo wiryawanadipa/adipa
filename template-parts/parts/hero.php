9c997694327e29d027c0dca618c21d5bf322a194
wrywndp <wiryawanadipa.trityavijaya@gmail.com>
2022-09-16 14:10:13 +0700

    hero

--- template-parts/parts/hero.php
+++ template-parts/parts/hero.php
@@ -1,6 +1,11 @@
 <div class="hero py-5">
 	<div class="herobox container-xl py-0 py-sm-5">
 		<div class="row py-4 py-sm-5">
+			<div class="col-lg-4 d-none d-lg-block">
+				<div class="hero-photo">
+					<img src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==" width="192px" height="192px" alt="wa logo big">
+				</div>
+			</div>
 			<div class="col-lg-8 p-xl-3">
 				<div class="hero-heading">
 					<div class="hero-heading-name">
@@ -11,11 +16,6 @@
 						<a href="<?php echo esc_url( get_category_link( get_cat_ID( 'Design Gallery' ) ) ); ?>" title="Design Gallery">design-gallery</a>
 						<a href="<?php echo esc_url( get_category_link( get_cat_ID( 'Project' ) ) ); ?>" title="Project">project</a>
 					</div>
-				</div>
-			</div>
-			<div class="col-lg-4 d-none d-lg-block">
-				<div class="hero-photo">
-					<img src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==" width="192px" height="192px" alt="wa logo big">
 				</div>
 			</div>
 		</div>
