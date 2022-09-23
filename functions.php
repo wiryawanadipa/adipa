ea6a7c665053a982c92a3cc31fa15dc04050cffc
wrywndp <wiryawanadipa.trityavijaya@gmail.com>
2022-09-23 09:47:53 +0700

    remove site.manifest

--- functions.php
+++ functions.php
@@ -69,7 +69,6 @@ function add_site_favicon() {
 	echo '<link rel="apple-touch-icon" sizes="180x180" href="' . get_stylesheet_directory_uri() . '/assets/favicon/apple-touch-icon.png">';
 	echo '<link rel="icon" type="image/png" sizes="32x32" href="' . get_stylesheet_directory_uri() . '/assets/favicon/favicon-32x32.png">';
 	echo '<link rel="icon" type="image/png" sizes="16x16" href="' . get_stylesheet_directory_uri() . '/assets/favicon/favicon-16x16.png">';
-	echo '<link rel="manifest" href="' . get_stylesheet_directory_uri() . '/assets/favicon/site.webmanifest">';
 }
 add_action('login_head', 'add_site_favicon');
 add_action('admin_head', 'add_site_favicon');
