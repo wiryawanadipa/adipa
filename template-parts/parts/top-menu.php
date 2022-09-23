4b97f43c4775dbd2199df97466ee6a6ee34033d7
wrywndp <wiryawanadipa.trityavijaya@gmail.com>
2022-09-23 22:12:37 +0700

    top-menu style

--- template-parts/parts/top-menu.php
+++ template-parts/parts/top-menu.php
@@ -6,21 +6,21 @@
 				<span class="navbar-toggler-icon"></span>
 			</button>
 			<div class="collapse navbar-collapse mt-1 mt-md-0" id="navbarSupportedContent">
-				<ul class="navbar-nav ms-auto mb-2 mb-md-0 mt-4 mt-md-0">
-					<li class="nav-item mx-md-1">
+				<ul class="navbar-nav ms-auto mt-3 mt-md-0">
+					<li class="nav-item">
 						<a class="nav-link fs-5 wa-top-menu<?php if(is_home()) { echo ' menu-active" aria-current="page'; } ?>" href="<?php bloginfo( 'url' ); ?>">home</a>
 					</li>
-					<li class="nav-item mx-md-1">
+					<li class="nav-item">
 						<a class="nav-link fs-5 wa-top-menu<?php if(is_category( 'blog' )) { echo ' menu-active" aria-current="page'; } if(is_single() && has_category( 'blog',$post->ID )) { echo ' menu-active'; } ?>" href="<?php echo esc_url(get_category_link(get_cat_ID( 'Blog' ))); ?>">blog</a>
 					</li>
-					<li class="nav-item mx-md-1">
+					<li class="nav-item">
 						<a class="nav-link fs-5 wa-top-menu<?php if (is_page( 'about' )) { echo ' menu-active" aria-current="page'; } ?>" href="<?php echo esc_url(get_page_link(get_page_id_by_title( 'About' ))); ?>">about</a>
 					</li>
-					<li class="nav-item mx-md-1">
+					<li class="nav-item">
 						<a class="nav-link fs-5 wa-top-menu<?php if(is_page( 'contact' )) { echo ' menu-active" aria-current="page'; } ?>" href="<?php echo esc_url(get_page_link(get_page_id_by_title( 'Contact' ))); ?>">contact</a>
 					</li>
 					<li class="nav-item dropdown">
-						<a class="nav-link dropdown-toggle p-3 p-md-2" href="#" role="button" data-bs-toggle="dropdown" title="Search Button" aria-expanded="false"><i class="fa-solid fa-magnifying-glass"></i></a>
+						<a class="nav-link dropdown-toggle p-2" href="#" role="button" data-bs-toggle="dropdown" title="Search Button" aria-expanded="false"><i class="fa-solid fa-magnifying-glass"></i></a>
 						<ul class="dropdown-menu dropdown-menu-end wa-search" itemscope itemtype="https://schema.org/WebSite">
 						<meta itemprop="url" content="<?php bloginfo('url'); ?>"/>
 							<form action="<?php bloginfo('url'); ?>" role="search" method="get" itemprop="potentialAction" itemscope itemtype="https://schema.org/SearchAction">
