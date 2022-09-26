a39679a0207958e4e29336fddd6eff5b79504284
wrywndp <wiryawanadipa.trityavijaya@gmail.com>
2022-09-27 01:26:05 +0700

    remo bootstrap

--- template-parts/footer.php
+++ template-parts/footer.php
@@ -6,7 +6,7 @@
 					<div class="theme"><i class="fa-brands fa-square-github"></i><a rel="nofollow noopener noreferrer" title="Wiryawan Adipa Wordpress Theme GitHub Repository" href="https://github.com/wiryawanadipa/adipa" target="_blank">Theme by Wiryawan Adipa v<?php echo wp_get_theme()->get( 'Version' ); ?></a></div>
 				</div>
 				<div class="footer-right">
-					<div class="social-link">
+					<div class="social-page">
 						<a rel="nofollow noopener noreferrer" title="Wiryawan Adipa Twitter" href="https://twitter.com/wiryawanadipa" target="_blank"><i class="fa-brands fa-square-twitter fa-2x"></i></a>
 						<a rel="nofollow noopener noreferrer" title="Wiryawan Adipa Instagram" href="https://www.instagram.com/wiryawanadipa" target="_blank"><i class="fa-brands fa-square-instagram fa-2x"></i></a>
 						<a rel="nofollow noopener noreferrer" title="Wiryawan Adipa LinkedIn" href="https://www.linkedin.com/in/wiryawanadipa" target="_blank"><i class="fa-brands fa-linkedin fa-2x"></i></a>
