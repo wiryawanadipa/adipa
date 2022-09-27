ed6a8758917d785e9a0ea5915030590e9c590eb1
wrywndp <wiryawanadipa.trityavijaya@gmail.com>
2022-09-27 22:24:00 +0700

    optimize image

--- template-parts/header.php
+++ template-parts/header.php
@@ -8,7 +8,10 @@
 		<link rel="preconnect" href="https://www.google.com">
 		<link rel="preconnect" href="https://fonts.googleapis.com">
 		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-		<link href="https://fonts.googleapis.com/css2?family=Rubik&display=swap" rel="stylesheet">
+		<link media="print" onload="this.onload=null;this.removeAttribute('media');" href="https://fonts.googleapis.com/css2?family=Rubik&display=swap" rel="stylesheet">
+		<noscript>
+			<link href="https://fonts.googleapis.com/css2?family=Rubik&display=swap" rel="stylesheet">
+		</noscript>
 		<link rel="apple-touch-icon" sizes="180x180" href="<?php bloginfo( 'stylesheet_directory' ); ?>/assets/favicon/apple-touch-icon.png">
 		<link rel="icon" type="image/png" sizes="32x32" href="<?php bloginfo( 'stylesheet_directory' ); ?>/assets/favicon/favicon-32x32.png">
 		<link rel="icon" type="image/png" sizes="16x16" href="<?php bloginfo( 'stylesheet_directory' ); ?>/assets/favicon/favicon-16x16.png">
--- template-parts/parts/top-menu.php
+++ template-parts/parts/top-menu.php
@@ -1,7 +1,7 @@
 <header class="header">
 	<nav class="navigation">
 		<div class="top-menu">
-			<a class="logo" href="<?php bloginfo('url'); ?>" <?php if ( is_home() ) { echo 'aria-current="page" '; } ?>><img src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==" width="40px" height="40px" alt="Wiryawan Adipa Logo"><span class="logo-name"><?php bloginfo( 'name' ); ?></span></a>
+			<a class="logo" href="<?php bloginfo('url'); ?>" <?php if ( is_home() ) { echo 'aria-current="page" '; } ?>><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACgAAAAoCAMAAAC7IEhfAAAANlBMVEWUV2LnlKL0nazrO1rrO1rrO1rrO1rrO1rrO1r//v/99PX86ev52dz2wcbynKXueIbsWm/rO1pkAjK0AAAACXRSTlMAEB5Jf5nEye5gj1JpAAABRklEQVR42pWV3ZrCIAxEUfvDkAQ47/+ye0Ht6qeu7FzR9kAICdOUnnRbth3I+7bc0kddl8yD8nJ9i11WXrReXrlb5o3yywZWPmh9DrvxUdsj+Af3RK78qTP6jS86Mrrkb2C+zAQ+g1+Z0DWltMyAS0opz4D5SLnVsADwAFwONC8q0c/EF4CQ5EBVGU+VkEWMtyP2KEo3FcClClWqVerg0lmefQyq1OiSAqrUXTbWPsA9HYMmBVUmg5DTzJ9A7iAmx0qVOqY6JldXeQFDpSu6FE0GtCiSZCd4P8YquRoud1WoRV6bnWC+JwNjekjFgSKHJjn9SObsWZcCmqR25IZL3saGt99Sh9SBogAokheZJOvjwM+ubSUAYnxoJnlrJu/33p1tivk2m27c6aswf7mmr+u8AcxbyrxJTdveP4x03prnzf7L7+MHRcVMimhg4jMAAAAASUVORK5CYII=" width="40px" height="40px" alt="Wiryawan Adipa Small Logo"><span class="logo-name"><?php bloginfo( 'name' ); ?></span></a>
 			<div id="menu-icon" onclick="mobileMenu()">
 				<div class="hamburger">
 					<div class="bar"></div>
