d190062785fc36719f72afb72977bcfb609d993b
wrywndp <wiryawanadipa.trityavijaya@gmail.com>
2022-09-29 16:22:21 +0700

    change logo

--- template-parts/parts/top-menu.php
+++ template-parts/parts/top-menu.php
@@ -1,7 +1,7 @@
 <header class="header">
 	<nav class="navigation">
 		<div class="top-menu">
-			<a class="logo" href="<?php bloginfo('url'); ?>" <?php if ( is_home() ) { echo 'aria-current="page" '; } ?>><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAFAAAABQBAMAAAB8P++eAAAAFVBMVEVhbHDtLlbrOlnuWnX////4wMrrO1rMgfNcAAAAAXRSTlMAQObYZgAAAe9JREFUSMe1l0tyhCAQhmfFSaZygETnALHJBUTX1KB9/yOkH4iOjtqpMiwU8fPvB4jt7bZuKO120hyWFo84fGlWbp9EtJGINhLRRrr3YDRyWxJ3m1FwLYlok3RHYDQKLiXdMRiNggtJK+jOwGgULJJW0J2D8W8gotG2FXQWMP4FRDTanroPE/hM4YfPXUuHGvj4rAP4dgbFRRoDBhN4PgA8GAftZCcFrGioofPIINJTPXWlNVuQkUEkBgbJ7ueoowuQzXl9oGdd6EfpkfYKzEOhgJVcV8XJOGVHjYLEROCjWriiYeezmErqPfVRFe874Kgx3ekw7IGJjQ4grlYkmyT19xJMAZGNBhCngiQ/e/6xBilvGLx4IKHzfHXhjSKFmaAhD/qkd8cAMCd8BsnKSCbJVZ3HPIVbkG7UMnNNxS5ypnwb3oNBAvF84jy2HOEWZAmYl0fIC24LohKDTk6apsjjVwbdnJ98j5ND4LfIe9RcxRewL8tbFCWgWrO/AHUtTv4T0wWY34YFOOpQnj+FQsnkC+iVEJ/k1Wqzx7oDlPzou1l1etlJIgO0mw0g7wDpdKc43VKu382u33H/4atw/ZfL/tG8/nttLxXMxYe9nDEXSPaSy1zE2ctCc6FpL13txbC9vLYX7PZfAPtPxcFvyi8BQrYEAw3olwAAAABJRU5ErkJggg==" alt="Wiryawan Adipa Small Logo"><span class="logo-name"><?php bloginfo( 'name' ); ?></span></a>
+			<a class="logo" href="<?php bloginfo('url'); ?>" <?php if ( is_home() ) { echo 'aria-current="page" '; } ?>><span class="logo-name"><span class="name-part">W</span>iryawan <span class="name-part">A</span>dipa</span></a>
 			<div id="menu-icon" onclick="mobileMenu()">
 				<div class="hamburger">
 					<div class="bar"></div>
