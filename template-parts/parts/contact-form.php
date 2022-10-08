112e7501153db0352b9dc914ca08a05617005942
wrywndp <wiryawanadipa.trityavijaya@gmail.com>
2022-10-08 22:14:56 +0700

    remove icon and remove </i>

--- template-parts/parts/contact-form.php
+++ template-parts/parts/contact-form.php
@@ -1,6 +1,4 @@
 <?php
-$exclamation = '<svg xmlns="http://www.w3.org/2000/svg" height=\'16px\' width=\'16px\' viewBox="0 0 512 512"><path d="M256 32c14.2 0 27.3 7.5 34.5 19.8l216 368c7.3 12.4 7.3 27.7 .2 40.1S486.3 480 472 480H40c-14.3 0-27.6-7.7-34.7-20.1s-7-27.8 .2-40.1l216-368C228.7 39.5 241.8 32 256 32zm0 128c-13.3 0-24 10.7-24 24V296c0 13.3 10.7 24 24 24s24-10.7 24-24V184c0-13.3-10.7-24-24-24zm32 224c0-17.7-14.3-32-32-32s-32 14.3-32 32s14.3 32 32 32s32-14.3 32-32z"/></svg>';
-$envelope = '<svg xmlns="http://www.w3.org/2000/svg" height=\'16px\' width=\'16px\' viewBox="0 0 512 512"><path d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"/></svg>';
 if (null != get_option('wa_recaptcha_site_key') && !empty(get_option('wa_recaptcha_site_key')) && null != get_option('wa_recaptcha_secret_key') && !empty(get_option('wa_recaptcha_secret_key')) && null != get_option('wa_mail') && !empty(get_option('wa_mail'))) {
 	$maxMessageChar = 1000;
 	if (isset($_POST['submit'])) {
@@ -50,45 +48,45 @@ if (null != get_option('wa_recaptcha_site_key') && !empty(get_option('wa_recaptc
 					$headers = 'From: ' . $name . ' <wordpress@' . $_SERVER['SERVER_NAME'] . '>' . "\r\n" . 'Reply-To: ' . $email;
 					$mail = wp_mail($emailTo, $subject, $body, $headers);
 					if ($mail) {
-						echo '<div class="info success">' . $envelope . 'Thank you for contacting me! Your message has been sent. I&lsquo;ll respond your message within 2x24 hours.</div>';
+						echo '<div class="info success">Thank you for contacting me! Your message has been sent. I&lsquo;ll respond your message within 2x24 hours.</div>';
 						$emailSent = true;
 					} else {
-						echo '<div class="info danger">' . $exclamation . '</i>Message was not sent. There is a problem with the server right now. Please try again. If the problem still persist you could contact me directly via social media.</div>';
+						echo '<div class="info danger">Message was not sent. There is a problem with the server right now. Please try again. If the problem still persist you could contact me directly via social media.</div>';
 					}
 				} else {
-					echo '<div class="info danger">' . $exclamation . '</i>Invalid captcha.</div>';
+					echo '<div class="info danger">Invalid captcha.</div>';
 				}
 			} else {
 				if (isset($emptyNameError)) {
-					echo '<div class="info danger">' . $exclamation . '</i>Please enter your name.</div>';
+					echo '<div class="info danger">Please enter your name.</div>';
 				}
 				if (isset($longNameError)) {
-					echo '<div class="info danger">' . $exclamation . '</i>The name that you filled in the name form is too long. The name form should be no more than 50 characters.</div>';
+					echo '<div class="info danger">The name that you filled in the name form is too long. The name form should be no more than 50 characters.</div>';
 				}
 				if (isset($emptyEmailError)) {
-					echo '<div class="info danger">' . $exclamation . '</i> Please enter your email address.</div>';
+					echo '<div class="info danger"> Please enter your email address.</div>';
 				}
 				if (isset($invalidEmailError)) {
-					echo '<div class="info danger">' . $exclamation . '</i>You entered an invalid email format address. (e.g. yourname@domain.tld).</div>';
+					echo '<div class="info danger">You entered an invalid email format address. (e.g. yourname@domain.tld).</div>';
 				}
 				if (isset($longEmailError)) {
-					echo '<div class="info danger">' . $exclamation . '</i>Your email address is too long. Use an email address which is no more than 80 characters.</div>';
+					echo '<div class="info danger">Your email address is too long. Use an email address which is no more than 80 characters.</div>';
 				}
 				if (isset($emptyMessageError)) {
-					echo '<div class="info danger">' . $exclamation . '</i>Please enter a message.</div>';
+					echo '<div class="info danger">Please enter a message.</div>';
 				}
 				if (isset($longMessageError)) {
-					echo '<div class="info danger">' . $exclamation . '</i>Your message is too long. Message should be no more than' . $maxMessageChar . 'characters.</div>';
+					echo '<div class="info danger">Your message is too long. Message should be no more than' . $maxMessageChar . 'characters.</div>';
 				}
 				if (!isset($emptySubjectError)) {
-					echo '<div class="info danger">' . $exclamation . '</i>SPAMMER!</div>';
+					echo '<div class="info danger">SPAMMER!</div>';
 				}
 				if (empty($_POST['g-recaptcha-response'])) {
-					echo '<div class="info danger">' . $exclamation . '</i>Please check the captcha.</div>';
+					echo '<div class="info danger">Please check the captcha.</div>';
 				}
 			}
 		} elseif ($_SESSION['rand'] != $_POST['randcheck']) {
-			echo '<div class="info danger">' . $exclamation . 'Please fill the form and check the captcha</div>';
+			echo '<div class="info danger">Please fill the form and check the captcha</div>';
 		}
 	}
 	?>
@@ -165,6 +163,6 @@ if (null != get_option('wa_recaptcha_site_key') && !empty(get_option('wa_recaptc
 	</form>
 <?php
 } else {
-	echo '<div class="info danger">' . $exclamation . '</i> Failed to create contact form.</div>';
-	echo '<div class="info danger">' . $exclamation . '</i> Please set your reCAPTCHA site key, secret key and add your email address in Theme Settings.</div>';
+	echo '<div class="info danger">Failed to create contact form.</div>';
+	echo '<div class="info danger">Please set your reCAPTCHA site key, secret key and add your email address in Theme Settings.</div>';
 }
