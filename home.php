8cbd7ffed175b0c6cc2e3e36f6c943e85c1a3d13
wrywndp <wiryawanadipa.trityavijaya@gmail.com>
2022-11-10 00:38:32 +0700

    revert main color

--- home.php
+++ home.php
@@ -2,7 +2,7 @@
 		<div class="homepage">
 			<div class="hero">
 				<div class="hero-photo">
-					<svg xmlns="http://www.w3.org/2000/svg" xml:space="preserve" width="192" height="192" version="1.1" viewBox="0 0 50.8 50.8"><g stroke-width=".265"><path d="M50.8 25.4a25.4 25.4 0 0 1-25.4 25.4A25.4 25.4 0 0 1 0 25.4 25.4 25.4 0 0 1 25.4 0a25.4 25.4 0 0 1 25.4 25.4Z" fill="#7b1289"/><path d="M8.649 33.401a77.15 77.15 0 0 1-.457-1.98l-.686-2.795-.762-3.2-2.057-4.905 5.055-3.855 3.606 17.475H8.827zm3.988-8 1.905-4.999 5.207-3.74 4.165 17.476H19.57l-.203-.838-.533-2.21-.762-3.225a250.435 250.435 0 0 1-.915-3.836h-.153l-.861 3.836-.28 1.295c-.085.355-.17.694-.254 1.016a40.535 40.535 0 0 1-.203.915l-.508 2.213-.205.837h-.28zm10.26-.153 1.525-5.38 5.055-3.205-2.082 8.762-.763 3.2a98.959 98.959 0 0 0-.66 2.795l-.483 1.98-.178.738h-.305zm11.43-8.585h.433l2.007 6.96-2.921 10.515h-5.08zm5.716 14.376H35.8l1.067-3.912H38.9l-2.997-7.97 4.648-2.494 5.562 17.475h-5.181z" fill="#fff"/></g></svg>
+					<svg xmlns="http://www.w3.org/2000/svg" xml:space="preserve" width="192" height="192" version="1.1" viewBox="0 0 50.8 50.8"><g stroke-width=".265"><path d="M50.8 25.4a25.4 25.4 0 0 1-25.4 25.4A25.4 25.4 0 0 1 0 25.4 25.4 25.4 0 0 1 25.4 0a25.4 25.4 0 0 1 25.4 25.4Z" fill="#eb3b5a"/><path d="M8.649 33.401a77.15 77.15 0 0 1-.457-1.98l-.686-2.795-.762-3.2-2.057-4.905 5.055-3.855 3.606 17.475H8.827zm3.988-8 1.905-4.999 5.207-3.74 4.165 17.476H19.57l-.203-.838-.533-2.21-.762-3.225a250.435 250.435 0 0 1-.915-3.836h-.153l-.861 3.836-.28 1.295c-.085.355-.17.694-.254 1.016a40.535 40.535 0 0 1-.203.915l-.508 2.213-.205.837h-.28zm10.26-.153 1.525-5.38 5.055-3.205-2.082 8.762-.763 3.2a98.959 98.959 0 0 0-.66 2.795l-.483 1.98-.178.738h-.305zm11.43-8.585h.433l2.007 6.96-2.921 10.515h-5.08zm5.716 14.376H35.8l1.067-3.912H38.9l-2.997-7.97 4.648-2.494 5.562 17.475h-5.181z" fill="#fff"/></g></svg>
 				</div>
 				<h1>WIRYAWAN ADIPA</h1>
 				<div class="hero-heading-text">Front-End Web Dev &bull; UX Designer &bull; WordPress Dev</div>
@@ -39,7 +39,7 @@
 					</form>
 				</div>
 				<div class="hero-footer">
-					<div><i class="fa-solid fa-copyright"></i> <?php echo date('Y'); ?>, <?php echo $_SERVER['HTTP_HOST']; ?></div>
+					<div><i class="fa-solid fa-copyright"></i> <?php echo date('Y'); ?> - <?php echo $_SERVER['HTTP_HOST']; ?></div>
 					<div><i class="fa-solid fa-scale-balanced"></i> <a href="<?php echo esc_url(get_page_link(get_page_by_title('Privacy Policy')->ID)); ?>" title="Privacy Policy" aria-label="Privacy Policy">Privacy Policy</a></div>
 					<div><i class="fa-brands fa-square-github"></i> <a rel="nofollow noopener noreferrer" title="Wiryawan Adipa Wordpress Theme GitHub Repository" aria-label="Wiryawan Adipa Wordpress Theme GitHub Repository" href="https://github.com/wiryawanadipa/adipa" target="_blank">Theme by Wiryawan Adipa</a></div>
 				</div>
