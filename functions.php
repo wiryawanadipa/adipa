0425bb6801ed98f1c36037e8b7238decfa3f29f5
wrywndp <wiryawanadipa.trityavijaya@gmail.com>
2022-11-10 18:23:02 +0700

    change font

--- functions.php
+++ functions.php
@@ -184,7 +184,7 @@ add_action('admin_head', 'wa_custom_font');
 function wa_custom_font() {
 	echo '<link rel="preconnect" href="https://fonts.googleapis.com">' . "\n\r";
 	echo '<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>' . "\n\r";
-	echo '<link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet">' . "\n\r";
+	echo '<link href="https://fonts.googleapis.com/css2?family=Audiowide&family=Roboto+Condensed:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">' . "\n\r";
 }
 
 
