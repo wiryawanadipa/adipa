f32d88d67718675d79b32b711cdeeec1e2e6ba0e
wrywndp <wiryawanadipa.trityavijaya@gmail.com>
2022-11-11 12:12:47 +0700

    update

--- search.php
+++ search.php
@@ -4,10 +4,10 @@
 	<?php if (have_posts()) { ?>
 		<div class="content-list search-content-list">
 			<h1>Search results for <span><?php echo get_search_query(); ?></span></h1>
-			<div class="search-box">
+			<div class="search-form">
 				<form action="<?php bloginfo('url'); ?>" role="search" method="get">
-					<input type="search" name="s" autocomplete="off" placeholder="Search here..." title="Search" aria-label="Search" required>
-					<button type="submit" aria-label="Search Button"><svg xmlns="http://www.w3.org/2000/svg" height='20px' width='20px' viewBox="0 0 512 512"><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352c79.5 0 144-64.5 144-144s-64.5-144-144-144S64 128.5 64 208s64.5 144 144 144z"/></svg></button>
+					<input type="search" name="s" autocomplete="off" placeholder="Search" title="Search" aria-label="Search" required>
+					<button type="submit" aria-label="Search Button"><i class="fa-solid fa-magnifying-glass"></i></button>
 				</form>
 			</div>
 			<?php
