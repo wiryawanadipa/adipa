3b60dafd9277401e7821718633a410978760ce5a
wrywndp <wiryawanadipa.trityavijaya@gmail.com>
2022-11-25 18:40:34 +0700

    remove preconnect

--- template-parts/header.php
+++ template-parts/header.php
@@ -9,7 +9,6 @@
 		<link rel="icon" type="image/png" sizes="16x16" href="<?php bloginfo('stylesheet_directory'); ?>/assets/favicon/favicon-16x16.png">
 		<link rel="preconnect" href="https://fonts.googleapis.com">
 		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-		<link rel="preconnect" href="https://cdnjs.cloudflare.com">
 		<link href="https://fonts.googleapis.com/css2?family=Audiowide&family=Roboto+Condensed:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
 		<meta name="theme-color" content="#eb3b5a" />
 		<?php wp_head(); ?>
