b5548f3e33f3960233893bae1cba3c4ef201beb5
wrywndp <wiryawanadipa.trityavijaya@gmail.com>
2022-11-25 18:35:04 +0700

    switch font-awesome webfont to svg

--- template-parts/header.php
+++ template-parts/header.php
@@ -11,7 +11,6 @@
 		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 		<link rel="preconnect" href="https://cdnjs.cloudflare.com">
 		<link href="https://fonts.googleapis.com/css2?family=Audiowide&family=Roboto+Condensed:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
-		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 		<meta name="theme-color" content="#eb3b5a" />
 		<?php wp_head(); ?>
 		<?php get_template_part('template-parts/parts/meta'); ?>
