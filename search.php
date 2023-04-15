a90f1a961ca9ae9cf97f41c645542ea247c691a9
wrywndp <wiryawanadipa.trityavijaya@gmail.com>
2023-04-15 13:57:00 +0700

    fixed robot icon

--- search.php
+++ search.php
@@ -25,7 +25,7 @@
 			<div class="not-found">
 				<div class="not-found-info">
 					<h1>No <span><?php echo get_search_query(); ?></span> found</h1>
-					<i class="fa-solid fa-robot fa-10x"></i>
+					<svg xmlns="http://www.w3.org/2000/svg" fill="#fc5c65" height="192px" width="192px" viewBox="0 0 640 512"><path d="M320 0c17.7 0 32 14.3 32 32v64h120c39.8 0 72 32.2 72 72v272c0 39.8-32.2 72-72 72H168c-39.8 0-72-32.2-72-72V168c0-39.8 32.2-72 72-72h120V32c0-17.7 14.3-32 32-32zM208 384c-8.8 0-16 7.2-16 16s7.2 16 16 16h32c8.8 0 16-7.2 16-16s-7.2-16-16-16h-32zm96 0c-8.8 0-16 7.2-16 16s7.2 16 16 16h32c8.8 0 16-7.2 16-16s-7.2-16-16-16h-32zm96 0c-8.8 0-16 7.2-16 16s7.2 16 16 16h32c8.8 0 16-7.2 16-16s-7.2-16-16-16h-32zM264 256a40 40 0 1 0-80 0 40 40 0 1 0 80 0zm152 40a40 40 0 1 0 0-80 40 40 0 1 0 0 80zM48 224h16v192H48c-26.5 0-48-21.5-48-48v-96c0-26.5 21.5-48 48-48zm544 0c26.5 0 48 21.5 48 48v96c0 26.5-21.5 48-48 48h-16V224h16z"/></svg>
 					<p>Try another keyword.</p>
 				</div>
 				<div class="search-box search-page-type">
